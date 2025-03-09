c1ab9dec17606fb465a949c9fd8f15360d128f9f
Blaine Hatab <jbhatb@gmail.com>
2025-03-09 19:54:28 -0400

    stuff

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // Disable ESLint in production builds
+  eslint: {
+    // Only run ESLint in development, not in production
+    ignoreDuringBuilds: process.env.NODE_ENV === 'production',
+  },
+}
 
 module.exports = nextConfig
